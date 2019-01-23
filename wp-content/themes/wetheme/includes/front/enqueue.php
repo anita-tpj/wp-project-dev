75a16ea6fde424a12dbfaf21454bd39c2b71e092
anita-tpj <anita.tpj@gmail.com>
2019-01-23 15:36:37 +0100

    Fix bootstrap style path; Import fonts style; Import reboot style

--- wp-content/themes/wetheme/includes/front/enqueue.php
+++ wp-content/themes/wetheme/includes/front/enqueue.php
@@ -1,5 +1,5 @@
 <?php
 function we_enqueue() {
-    wp_register_style('we_bootstrap', get_template_directory_uri() . 'src/_vendor/_bootstrap/_bootstrap.css');
+    wp_register_style('we_bootstrap', get_template_directory_uri() . '/src/sass/_vendor/_bootstrap/bootstrap.css');
     wp_enqueue_style('we_bootstrap');
 }
