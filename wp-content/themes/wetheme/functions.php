5c0089c92a1c87587b04de8fc62206c155092dfd
anita-tpj <anita.tpj@gmail.com>
2019-01-23 13:33:26 +0100

    Add wp_enqueue_scripts hook and includes file structure for front end

--- wp-content/themes/wetheme/functions.php
+++ wp-content/themes/wetheme/functions.php
@@ -2,8 +2,12 @@
 
 //Set up
 
+
 //Includes 
+include(get_template_directory() . '/includes/front/enqueue.php');
 
 //Action & Filters Hooks
+add_action('wp_enqueue_scripts', 'we_enqueue');
+
 
 //Shortcodes
