c0fd0a45b9533a9ef0089126b5321910bca0b097
anita-tpj <anita.tpj@gmail.com>
2019-01-24 18:51:13 +0100

    Import fontAwesome ver 5.6.3

--- wp-content/themes/wetheme/404.php
+++ wp-content/themes/wetheme/404.php
@@ -4,7 +4,7 @@
     <div class="row">
         <div class="col-md-12 blog-main">
             <h2 class="text-center text-danger">
-                <i class="fa fa-frown-o"></i><br>
+                <i class="far fa-frown"></i><br>
                 <?php _e('404! Page not found!', 'wetheme'); ?>
             </h2>
         </div><!-- /.blog-main -->
