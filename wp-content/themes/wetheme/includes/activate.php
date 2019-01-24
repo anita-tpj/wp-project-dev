48340b8aef713a4003953704d9a5694d79fb6441
anita-tpj <anita.tpj@gmail.com>
2019-01-24 19:32:06 +0100

    Create theme options for social networks, logo and footer via Option API

--- wp-content/themes/wetheme/includes/activate.php
+++ wp-content/themes/wetheme/includes/activate.php
@@ -1,6 +1,22 @@
 <?php
+
 function we_activate() {
    if(version_compare(get_bloginfo('version'), '5.0.3', '<'))  {
        wp_die(__('You must have a minimum version of 5.0.3 to use this theme'));
    }
+}
+
+$theme_opts = get_option('we_opts');
+
+if(!$theme_opts) {
+    $opts               = array(
+        'facebook'      => '',
+        'twitter'       => '',
+        'youtube'       => '',
+        'logo_type'     => 1,
+        'logo_img'      => '',
+        'footer'        => ''
+    );
+
+    add_option('we_opts', $opts);
 }
