14cd3e223ca5b0e560ab00b5ff410047652801c6
anita-tpj <anita.tpj@gmail.com>
2019-01-24 14:18:55 +0100

    Add error page template

---
+++ wp-content/themes/wetheme/404.php
@@ -0,0 +1,16 @@
+<?php get_header(); ?>
+
+<main role="main" class="container">
+    <div class="row">
+        <div class="col-sm-12 blog-main">
+            <h2 class="text-center text-danger">
+                <i class="fa fa-frown-o"></i><br>
+                <?php _e('404! Page not found!', 'wetheme'); ?>
+            </h2>
+        </div><!-- /.blog-main -->
+
+    </div><!-- /.row -->
+
+</main><!-- /.container -->
+
+<?php get_footer(); ?>
