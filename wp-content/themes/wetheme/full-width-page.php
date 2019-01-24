79ebb8cee0c11076cfa47edb014bb5dea2995608
anita-tpj <anita.tpj@gmail.com>
2019-01-24 17:44:16 +0100

    Create custom full width template page

---
+++ wp-content/themes/wetheme/full-width-page.php
@@ -0,0 +1,36 @@
+<?php
+/*
+* Template Name: Full Width Page
+*/
+
+get_header(); ?>
+
+<main role="main" class="container">
+<div class="row">
+<div class="col-md-12 blog-main">
+
+    <?php
+        if(have_posts()) {
+            while(have_posts()) {
+                the_post(); ?>
+
+                <div class="blog-post">
+
+                    <h2 class="blog-post-title"><?php the_title(); ?></h2>
+                    
+                    <p class="blog-post-content"><?php the_content(); ?></p>
+                   
+                </div><!-- /.blog-post -->
+                <?php comments_template(); ?>
+   <?php
+            }
+        }
+    ?>
+
+</div><!-- /.blog-main -->
+
+</div><!-- /.row -->
+
+</main><!-- /.container -->
+
+<?php get_footer(); ?>
