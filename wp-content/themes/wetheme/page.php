48256b866462794a455d6e901691d014276cf533
anita-tpj <anita.tpj@gmail.com>
2019-01-24 14:12:50 +0100

    Create page template

---
+++ wp-content/themes/wetheme/page.php
@@ -0,0 +1,34 @@
+<?php get_header(); ?>
+
+<main role="main" class="container">
+<div class="row">
+<div class="col-md-8 blog-main">
+
+    <?php
+        if(have_posts()) {
+            while(have_posts()) {
+                the_post(); ?>
+
+                <div class="blog-post">
+
+                    <h2 class="blog-post-title"><?php the_title(); ?></h2>
+                    
+                    <p class="blog-post-content"><?php the_content(); ?></p>
+                   
+                </div><!-- /.blog-post -->
+   <?php
+            }
+        }
+    ?>
+
+</div><!-- /.blog-main -->
+
+<aside class="col-md-4 blog-sidebar">
+    <?php get_sidebar(); ?>
+</aside><!-- /.blog-sidebar -->
+
+</div><!-- /.row -->
+
+</main><!-- /.container -->
+
+<?php get_footer(); ?>
