9665151c6a10f7df7b5e2751e938f5f104c26171
anita-tpj <anita.tpj@gmail.com>
2019-01-31 17:29:14 +0100

    Add localization functions for internationalization and easy translate of comment form

--- wp-content/themes/wetheme/comments.php
+++ wp-content/themes/wetheme/comments.php
@@ -11,32 +11,32 @@
 
 <?php
 if(comments_open()) : ?>
-    <h4>Leave a Comment</h4>
+    <h4><?php _e('Leave a Comment', 'wetheme'); ?></h4>
     <form action="<?php echo site_url('wp-comments-post.php'); ?>" method="post" id="commentsform">
         <input type='hidden' name='comment_post_ID' value='<?php echo $post->ID; ?>' id='comment_post_ID' />
 
     <div class="form-group">
-        <label for="aouthor">Name / Alias (required)</label>
+        <label for="aouthor"><?php _e('Name / Alias (required)', 'wetheme'); ?></label>
         <input type="text" class="form-control" name ="author" id="author" placeholder="John Doe" required>
     </div>
 
     <div class="form-group">
-    <label for="email">Email address (required, not showen)</label>
+    <label for="email"><?php _e('Email address (required, not showen)', 'wetheme'); ?></label>
     <input type="email" class="form-control" name="email" id="email" placeholder="name@example.com" required>
     </div>
 
     <div class="form-group">
-    <label for="url">Website (optional)</label>
+    <label for="url"><?php _e('Website (optional)', 'wetheme'); ?></label>
     <input type="url" class="form-control" name="url" id="url" placeholder="http://example.com">
     </div>
 
     <div class="form-group">
-    <label for="comments">Comment</label>
+    <label for="comments"><?php _e('Comment', 'wetheme'); ?></label>
     <textarea class="form-control" name = "comment" id="comments" rows="7" cols='60' required></textarea>
     </div>
 
     <div class="form-group">
-    <button type="submit" class="btn btn-primary">Add Comment</button>
+    <button type="submit" class="btn btn-primary"><?php _e('Add Comment', 'wetheme'); ?></button>
     </div>
     </form>
 <?php
