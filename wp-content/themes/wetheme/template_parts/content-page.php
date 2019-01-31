7ac31ceeb9da87f3427d25c88779312dc67e8a01
anita-tpj <anita.tpj@gmail.com>
2019-01-31 16:01:38 +0100

    Create folder for template parts; Create template part for pages

---
+++ wp-content/themes/wetheme/template_parts/content-page.php
@@ -0,0 +1,6 @@
+
+<div class="blog-post">
+    <h2 class="blog-post-title"><?php the_title(); ?></h2>
+    <p class="blog-post-content"><?php the_content(); ?></p>
+</div><!-- /.blog-post -->
+<?php comments_template(); ?>
