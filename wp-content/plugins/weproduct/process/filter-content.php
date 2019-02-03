38d88f873f59df818bad29b2894df426984a7d8a
anita-tpj <anita.tpj@gmail.com>
2019-02-03 18:44:16 +0100

    Make plugin sting able

--- wp-content/plugins/weproduct/process/filter-content.php
+++ wp-content/plugins/weproduct/process/filter-content.php
@@ -18,5 +18,14 @@ function wep_filter_product_content($content) {
     $weproduct_html = str_replace('STOCK_PH', $weproduct_data['stock'], $weproduct_html);
 
 
+    $weproduct_html = str_replace('NAME_I18N', __('Product Name', 'weproduct'), $weproduct_html);
+    $weproduct_html = str_replace('DESC_I18N', __('Product Description', 'weproduct'), $weproduct_html);
+    $weproduct_html = str_replace('DETAILS_I18N', __('Product Details', 'weproduct'), $weproduct_html);
+    $weproduct_html = str_replace('TYPE_I18N', __('Product Type', 'weproduct'), $weproduct_html);
+    $weproduct_html = str_replace('IMAGE_I18N', __('Product Image', 'weproduct'), $weproduct_html);
+    $weproduct_html = str_replace('PRICE_I18N', __('Product Price', 'weproduct'), $weproduct_html);
+    $weproduct_html = str_replace('SKU_I18N', __('Product SKU', 'weproduct'), $weproduct_html);
+    $weproduct_html = str_replace('STOCK_I18N', __('Product Stock', 'weproduct'), $weproduct_html);
+
     return $weproduct_html . $content;
 }
--- wp-content/plugins/weproduct/process/weproduct-template.php
+++ wp-content/plugins/weproduct/process/weproduct-template.php
@@ -1,10 +1,10 @@
 <section class="weproduct">
-    <div><strong>Product Name: </strong>NAME_PH</div>
-    <div><strong>Product Description: </strong>DESC_PH</div>
-    <div><strong>Product Details: </strong>DETAILS_PH</div>
-    <div><strong>Product Type: </strong>TYPE_PH</div>
-    <div><strong>Product Image: </strong>IMAGE_PH</div>
-    <div><strong>Product Price: </strong>PRICE_PH</div>
-    <div><strong>Product SKU: </strong>SKU_PH</div>
-    <div><strong>Product Stock: </strong>STOCK_PH</div>
+    <div><strong>NAME_I18N: </strong>NAME_PH</div>
+    <div><strong>DESC_I18N: </strong>DESC_PH</div>
+    <div><strong>DETAILS_I18N: </strong>DETAILS_PH</div>
+    <div><strong>TYPE_I18N: </strong>TYPE_PH</div>
+    <div><strong>IMAGE_I18N: </strong>IMAGE_PH</div>
+    <div><strong>PRICE_I18N: </strong>PRICE_PH</div>
+    <div><strong>SKU_I18N: </strong>SKU_PH</div>
+    <div><strong>STOCK_I18N: </strong>STOCK_PH</div>
 </section>
